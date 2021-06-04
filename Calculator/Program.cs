f9e42c89afa1f54fb3f11711f179a034c291d8c7
payalbhalala <pkb7@njit.edu>
2021-06-04 12:01:58 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
